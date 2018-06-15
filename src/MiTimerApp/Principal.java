474a25ec1c5afbf67e2a0979e7faef40b616a8d6
Albeto A. Del Yesso <aa_dy@hotmail.com>
2018-06-15 10:33:34 -0300

    proyecto

---
+++ src/MiTimerApp/Principal.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alberto del yesso
+ */
+
+package MiTimerApp;
+
+
+public class Principal {
+    
+public static void main(String[] args) {
+    ventanaMenu unaVentanaMenu = new ventanaMenu();
+    unaVentanaMenu.setVisible(true);
+}    
+    
+     
+        
+}
